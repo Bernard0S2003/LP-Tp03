3b9b1afbebe243768fd13eaebc815f67cd6dc6fa
Bernardo Silva <bernardo.alexandre.silva@outlook.pt>
2026-05-07 14:54:08 +0100

    Second Commit

---
+++ src/main/java/pt/ua/estga/lp/batalha_naval/Batalha_Naval.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pt.ua.estga.lp.batalha_naval;
+
+/**
+ *
+ * @author bernardos
+ */
+public class Batalha_Naval {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
